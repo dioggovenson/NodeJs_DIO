3a2bba2595bbe6717464c1e80239e50555a87361
Dioggo Venson <dioggovenson@gmail.com>
2022-02-21 20:02:37 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import  {Pool} from 'pg';
 
-const connectionString = 'postgres://fooxrzxp:o7p2LF6mWbjHkqJiPK_9EmtCxm8C_2Ui@jelani.db.elephantsql.com/fooxrzxp';
+const connectionString = 'link';
 
 const db = new Pool({connectionString});
 
